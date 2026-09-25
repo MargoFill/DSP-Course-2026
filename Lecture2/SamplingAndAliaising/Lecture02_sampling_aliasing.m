4561ddf25e4e92831fb3845c508347254cb89a87
Margarita Filenko <fil7marg@gmail.com>
2026-09-25 11:16:54 +0300

    Update Lecture02_sampling_aliasing.m

--- Lecture2/SamplingAndAliaising/Lecture02_sampling_aliasing.m
+++ Lecture2/SamplingAndAliaising/Lecture02_sampling_aliasing.m
@@ -13,44 +13,6 @@ fs_sampling_list= [15,20,25,50,100];
  t = 0:1/fs_original:duration-1/fs_original;
  
  x = amplitude * sin(2*pi*signal_frequency*t);
- %x1 = amplitude * sin(2*pi*f_sampling*t);
-
-% %{ Listen to the original signal
-% 
-% disp('Original signal: 7 kHz');
-% 
-% % soundsc(x, fs_original);
-% % pause(duration + 1);
-% % 
-% %  Select a new sampling frequency
-% % 
-% % % Change only this value
-% % fs_low = 24000;
-% % 
-% % % The original frequency must be divisible by fs_low
-% % M = fs_original / fs_low;
-% % 
-% % if mod(M,1) ~= 0
-% %     error('Choose an fs_low value that divides 48000 exactly.');
-% % end
-% % 
-% %  Downsample without an anti-aliasing filter
-% % 
-% % x_alias = x(1:M:end);
-% % 
-% % nyquist_frequency = fs_low / 2;
-% % 
-% % fprintf('New sampling frequency: %.0f Hz\n', fs_low);
-% % fprintf('New Nyquist frequency: %.0f Hz\n', nyquist_frequency);
-% % 
-% %  Listen to the downsampled signal
-% % 
-% % disp('Downsampled signal');
-% % 
-% % soundsc(x_alias, fs_low);
-% % 
-% % %% Compare the frequency spectra
-% % }%
 
 figure;
 
